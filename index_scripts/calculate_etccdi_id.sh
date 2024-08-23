5062c9331a5667925f03c97e5284bde1c78d7caa
Lukas Brunner <lukas.brunner@live.at>
2024-08-23 16:50:21 +0200

    Revert debugging change

--- index_scripts/calculate_etccdi_id.sh
+++ index_scripts/calculate_etccdi_id.sh
@@ -24,6 +24,6 @@ else
     mm="year"
 fi
 
-cdo etccdi_id,freq=year $infile ${outfile}.nc || { echo "ERROR"; exit 1; }
+cdo etccdi_id,freq=$mm $infile ${outfile}.nc || { echo "ERROR"; exit 1; }
 echo "$(date +"%Y-%m-%d %H:%M:%S") - Calculated $index"
 echo ${outfile}.nc
